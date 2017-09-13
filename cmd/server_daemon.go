602ebc788908762436f726108224e72800cb6bf1
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2017-09-13 19:28:20 +0200

    Format code

--- cmd/server_daemon.go
+++ cmd/server_daemon.go
@@ -3,29 +3,29 @@ package main
 import (
 	"time"
 
-	"github.com/deshboard/boilerplate-crondaemon/app"
+	. "github.com/deshboard/boilerplate-crondaemon/app"
 	"github.com/goph/serverz"
 )
 
 // newDaemonServer creates a new daemon server.
-func newDaemonServer(appCtx *application) serverz.Server {
+func newDaemonServer(app *application) serverz.Server {
 	var ticker *time.Ticker
 
-	if appCtx.config.Daemon {
-		ticker = time.NewTicker(appCtx.config.DaemonSchedule)
+	if app.config.Daemon {
+		ticker = time.NewTicker(app.config.DaemonSchedule)
 	}
 
 	return &serverz.AppServer{
 		Server: &serverz.DaemonServer{
 			Daemon: &serverz.CronDaemon{
-				Job: app.NewService(
-					app.Logger(appCtx.logger),
-					app.ErrorHandler(appCtx.errorHandler),
+				Job: NewService(
+					Logger(app.logger),
+					ErrorHandler(app.errorHandler),
 				),
 				Ticker: ticker,
 			},
 		},
 		Name:   "daemon",
-		Logger: appCtx.logger,
+		Logger: app.logger,
 	}
 }
